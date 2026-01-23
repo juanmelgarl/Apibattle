277a6094426b131933ccf966dd17d5be8784c257
juanmelgarl <juanmelgar8800@gmail.com>
2026-01-23 20:12:11 -0300

    Fix bugs in the Api
    Se encontraron Bugs en la api hacian que no se ejecutara correctamente

--- WebApplication14/Program.cs
+++ WebApplication14/Program.cs
@@ -1,5 +1,4 @@
 using FluentValidation;
-using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using WebApplication14.Command;
 using WebApplication14.Models;
@@ -9,32 +8,27 @@ using WebApplication14.Repository;
 var builder = WebApplication.CreateBuilder(args);
 
 
-
-builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddMemoryCache();
 
-builder.Services.AddDbContext<GotDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("GotDB")));
+builder.Services.AddDbContext<GotDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GotDB")));
+
 builder.Services.AddScoped<IBattleQuery, BattleQuery>();
 builder.Services.AddScoped<IBattleRepository, BattleRepository>();
 builder.Services.AddScoped<IbattleCommand, BattleCommand>();
-builder.Services
-    .AddControllers()
-    .AddFluentValidation(fv =>
-    {
-        fv.RegisterValidatorsFromAssemblyContaining<Program>();
-        fv.DisableDataAnnotationsValidation = true;
-    });
+
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 var app = builder.Build();
 
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
